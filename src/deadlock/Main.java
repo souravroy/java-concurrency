dfb14d2f9b28301a5f9e0918b34b418f5d23c1d7
Sourav Roy <sourav.roy@hmcts.net>
2022-05-28 17:34:09 +0100

    deadlock with synchronized block

--- src/deadlock/Main.java
+++ src/deadlock/Main.java
@@ -12,7 +12,13 @@ public class Main {
         Runnable1 runnable1 = new Runnable1(lock1, lock2);
         Runnable2 runnable2 = new Runnable2(lock1, lock2);
 
-        new Thread(runnable1).start();
-        new Thread(runnable2).start();
+        /*new Thread(runnable1).start();
+        new Thread(runnable2).start();*/
+
+        RunnableSync1 runnableSync1 = new RunnableSync1(lock1, lock2);
+        RunnableSync2 runnableSync2 = new RunnableSync2(lock1, lock2);
+
+        new Thread(runnableSync1).start();
+        new Thread(runnableSync2).start();
     }
 }
---
+++ src/deadlock/RunnableSync1.java
@@ -0,0 +1,35 @@
+package deadlock;
+
+public class RunnableSync1 implements Runnable{
+    Object lock1 = null;
+    Object lock2 = null;
+
+    public RunnableSync1(Object lock1, Object lock2) {
+        this.lock1 = lock1;
+        this.lock2 = lock2;
+    }
+
+    @Override
+    public void run() {
+        String threadName = Thread.currentThread().getName();
+
+        System.out.println(threadName + " attempting to lock lock1");
+        synchronized(lock1) {
+            System.out.println(threadName + " locked lock1");
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException ie) {
+                // ignore
+            }
+
+            System.out.println(threadName + " attempting to lock lock2");
+            synchronized(lock2) {
+                System.out.println(threadName + " locked lock2");
+                // do the work - now that both locks have been acquired.
+
+            }
+            System.out.println(threadName + " unlocking lock2");
+        }
+        System.out.println(threadName + " unlocking lock1");
+    }
+}
